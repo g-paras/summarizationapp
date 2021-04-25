507ca57177bc1af7cad272781043d5254367ae39
Paras Gupta <guptaparas039@gmail.com>
2021-04-25 14:47:37 +0530

    Error handling

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ from gensim.summarization import summarize
 
 
 st.title('Summarization app')
-
-text = st.text_area('Enter text here')
+st.info('_Input should have more than one paragraph_')
+text = st.text_area('Text to analyze', height=300)
 if st.button('Summarize'):
-    st.write(summarize(text))
+    if len(text) < 500:
+        st.error('Text should have more than 500 words')
+    else:
+        # text = text.replace('.', '\n')
+        try:
+            st.write(summarize(text))
+        except:
+            st.error('ValueError: Input must have more than one sentence')
+    # st.write(len(text))
